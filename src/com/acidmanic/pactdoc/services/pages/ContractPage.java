c057238ab0a2362d5cb3ad35d24f2a44ece7703f
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2019-02-28 17:42:21 +0330

    WIP: generating other types of documents

---
+++ src/com/acidmanic/pactdoc/services/pages/ContractPage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.pactdoc.services.pages;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public interface ContractPage {
+ 
+    
+    
+    void setTitle(String title);
+}
