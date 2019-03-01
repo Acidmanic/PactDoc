2a96344b6c2a196b8f96181e34663005b5de319a
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2019-03-01 12:23:43 +0330

    Move playgrounds to test packages

--- test/playgrounds/Playground.java
+++ test/playgrounds/Playground.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.acidmanic.pactdoc.playgrounds;
+package playgrounds;
 
 import com.acidmanic.pactdoc.services.ContractContentIndexer;
 import com.acidmanic.pactdoc.services.ContractIndexer;
