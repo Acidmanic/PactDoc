f074028e5d71c260e1ba576bbd8c2fa98e341324
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2019-03-01 15:09:31 +0330

    Move WikiGeneration logic to WikiGenerator class

--- src/com/acidmanic/pactdoc/services/wikigenerators/MarkdownWikiGenerator.java
+++ src/com/acidmanic/pactdoc/services/wikigenerators/MarkdownWikiGenerator.java
@@ -7,88 +7,31 @@ package com.acidmanic.pactdoc.services.wikigenerators;
 
 import com.acidmanic.pactdoc.services.contentproviders.MarkdownContentProvider;
 import com.acidmanic.pactdoc.services.contentproviders.ContentProvider;
-import com.acidmanic.pactdoc.models.Contract;
 import com.acidmanic.pactdoc.services.ContractIndexer;
-import com.acidmanic.pactdoc.services.Glossary;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  *
  * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
  */
 public class MarkdownWikiGenerator extends WikiGeneratorBase{
-    
-    
-    private final ContractIndexer indexer;
-    private final String linksBase;
-    private boolean  linksEndWithFileExtionsion;
 
-    public MarkdownWikiGenerator(ContractIndexer indexer, String linksBase, boolean generateFilesWithExtension) {
-        this.indexer = indexer;
-        this.linksBase = linksBase;
-        this.linksEndWithFileExtionsion = generateFilesWithExtension;
+    public MarkdownWikiGenerator(boolean linksEndWithFileExtionsion, ContractIndexer indexer, String linksBase) {
+        super(linksEndWithFileExtionsion, indexer, linksBase);
     }
+    
 
     public MarkdownWikiGenerator(ContractIndexer indexer, String linksBase) {
-        this.indexer = indexer;
-        this.linksBase = linksBase;
-        this.linksEndWithFileExtionsion=false;
+        super(false, indexer, linksBase);
     }
     
 
-
-    private Glossary makeGlossary(ContractIndexer indexer,String linksBase) {
-        
-        Glossary glossary = new Glossary();
-        
-        Path base = Paths.get(linksBase);
-        
-        String extension=linksEndWithFileExtionsion?".md":"";
-        
-        /*      Add home        */
-        
-        glossary.put(base.resolve("Index"+extension).toString(), "");
-        
-        /*  Add services links  */
-        for(String service:indexer.getServices()){
-            glossary.put( base.resolve(service+extension).toString()
-                    , service);
-        }
-        
-        /*  Add contracts   */
-        for(String service:indexer.getServices()){
-            for(Contract contract:indexer.getContracts(service)){
-                glossary.put(base.resolve(contract.getProvider()
-                        .getName()+extension).toString(), contract);
-            }
-        }
-        
-        return glossary;
-    }
-
-    @Override
-    protected Glossary createGlossary() {
-        return makeGlossary(indexer, linksBase);
-    }
-
-    @Override
-    protected boolean isLinksEndWithFileExtionsion() {
-        return linksEndWithFileExtionsion;
-    }
 
     @Override
     protected ContentProvider getContentProvider() {
-        return new MarkdownContentProvider(indexer);
+        return new MarkdownContentProvider(getIndexer());
     }
 
 
-    public void setLinksEndWithFileExtionsion(boolean linksEndWithFileExtionsion) {
-        this.linksEndWithFileExtionsion = linksEndWithFileExtionsion;
-    }
-    
-    
-    
     
     
 }
--- src/com/acidmanic/pactdoc/services/wikigenerators/WikiGeneratorBase.java
+++ src/com/acidmanic/pactdoc/services/wikigenerators/WikiGeneratorBase.java
@@ -5,6 +5,8 @@
  */
 package com.acidmanic.pactdoc.services.wikigenerators;
 
+import com.acidmanic.pactdoc.models.Contract;
+import com.acidmanic.pactdoc.services.ContractIndexer;
 import com.acidmanic.pactdoc.services.Glossary;
 import com.acidmanic.pactdoc.services.GlossaryScanner;
 import com.acidmanic.pactdoc.services.contentproviders.ContentProvider;
@@ -22,17 +24,33 @@ public abstract class WikiGeneratorBase {
     
     
     private boolean byVersion;
+    private final boolean  linksEndWithFileExtionsion;
+    private final ContractIndexer indexer;
+    private final String linksBase;
     
-    protected abstract Glossary createGlossary();
-    
-    protected abstract boolean isLinksEndWithFileExtionsion();
     
     protected abstract ContentProvider getContentProvider();
+
+    public WikiGeneratorBase(boolean linksEndWithFileExtionsion, ContractIndexer indexer, String linksBase) {
+        this.linksEndWithFileExtionsion = linksEndWithFileExtionsion;
+        this.indexer = indexer;
+        this.linksBase = linksBase;
+    }
+
+   
+
+    
+    
+    
+    public boolean getLinksEndWithFileExtension(){
+        return this.linksEndWithFileExtionsion;
+    }
+    
     
     public void generate(String destinationDirectory) {
         Glossary glossary = createGlossary();
         
-        final String extension = this.isLinksEndWithFileExtionsion()?"":".md";
+        final String extension = linksEndWithFileExtionsion?"":".md";
         
         final Path baseDirectory = Paths.get(destinationDirectory);
         
@@ -68,6 +86,37 @@ public abstract class WikiGeneratorBase {
         }
     }
 
+    
+    private Glossary createGlossary() {
+        
+        Glossary glossary = new Glossary();
+        
+        Path base = Paths.get(linksBase);
+        
+        String extension=getLinksEndWithFileExtension()?".md":"";
+        
+        /*      Add home        */
+        
+        glossary.put(base.resolve("Index"+extension).toString(), "");
+        
+        /*  Add services links  */
+        for(String service:indexer.getServices()){
+            glossary.put( base.resolve(service+extension).toString()
+                    , service);
+        }
+        
+        /*  Add contracts   */
+        for(String service:indexer.getServices()){
+            for(Contract contract:indexer.getContracts(service)){
+                glossary.put(base.resolve(contract.getProvider()
+                        .getName()+extension).toString(), contract);
+            }
+        }
+        
+        return glossary;
+    }
+    
+    
     public boolean isByVersion() {
         return byVersion;
     }
@@ -75,6 +124,20 @@ public abstract class WikiGeneratorBase {
     public void setByVersion(boolean byVersion) {
         this.byVersion = byVersion;
     }
+
+    public boolean isLinksEndWithFileExtionsion() {
+        return linksEndWithFileExtionsion;
+    }
+
+    public ContractIndexer getIndexer() {
+        return indexer;
+    }
+
+    public String getLinksBase() {
+        return linksBase;
+    }
+    
+    
     
     
     
