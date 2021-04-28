572818d1b8be9bad80f6b3c7925ac2f398246b76
Mani Moayedi <you@example.com>
2021-04-28 22:47:54 +0430

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+BASE=target/pact-doc
+
+rm -rf $BASE
+
+mvn clean install && \
+mkdir $BASE && \
+cp target/application-standalone.jar $BASE && \
+echo 'DIR=$(dirname $BASH_SOURCE)' > $BASE/run.sh && \
+echo 'DIR=$(realpath $DIR)' >> $BASE/run.sh && \
+echo 'JAR=$DIR/application-standalone.jar' >> $BASE/run.sh && \
+echo 'java -jar "$JAR" $@' >> $BASE/run.sh && \
+chmod +x $BASE/run.sh
+
